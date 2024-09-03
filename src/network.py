94077c4a4e061d6889f05504186c5860a81e9753
Viliam Vadocz <viliam.vadocz@gmail.com>
2024-09-03 21:38:42 +0200

    Add ResNet

--- src/network.py
+++ src/network.py
@@ -3,6 +3,111 @@ import jax
 import jax.numpy as jnp
 
 
+class BlockV1(hk.Module):
+    def __init__(self, num_channels, name="BlockV1"):
+        super(BlockV1, self).__init__(name=name)
+        self.num_channels = num_channels
+
+    def __call__(self, x, is_training, test_local_stats):
+        i = x
+        x = hk.Conv2D(self.num_channels, kernel_shape=3)(x)
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(self.num_channels, kernel_shape=3)(x)
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+        return jax.nn.relu(x + i)
+
+
+class BlockV2(hk.Module):
+    def __init__(self, num_channels, name="BlockV2"):
+        super(BlockV2, self).__init__(name=name)
+        self.num_channels = num_channels
+
+    def __call__(self, x, is_training, test_local_stats):
+        i = x
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(self.num_channels, kernel_shape=3)(x)
+        x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(self.num_channels, kernel_shape=3)(x)
+        return x + i
+
+
+class EpistemicAZNet(hk.Module):
+    """AlphaZero NN architecture."""
+
+    def __init__(
+        self,
+        num_actions,
+        num_channels: int = 16,  # FIXME: Make 64 again
+        num_blocks: int = 2,  # FIXME: Make 5 again
+        resnet_v2: bool = True,
+        name="az_net",
+    ):
+        super().__init__(name=name)
+        self.num_actions = num_actions
+        self.num_channels = num_channels
+        self.num_blocks = num_blocks
+        self.resnet_v2 = resnet_v2
+        self.resnet_cls = BlockV2 if resnet_v2 else BlockV1
+
+    def __call__(self, x, is_training, test_local_stats):
+        x = x.astype(jnp.float32)
+        x = hk.Conv2D(self.num_channels, kernel_shape=3)(x)
+
+        if not self.resnet_v2:
+            x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+            x = jax.nn.relu(x)
+
+        for i in range(self.num_blocks):
+            x = self.resnet_cls(self.num_channels, name=f"block_{i}")(x, is_training, test_local_stats)
+
+        if self.resnet_v2:
+            x = hk.BatchNorm(True, True, 0.9)(x, is_training, test_local_stats)
+            x = jax.nn.relu(x)
+
+        # policy head
+        main_policy_logits = hk.Conv2D(output_channels=2, kernel_shape=1)(x)
+        main_policy_logits = hk.BatchNorm(True, True, 0.9)(main_policy_logits, is_training, test_local_stats)
+        main_policy_logits = jax.nn.relu(main_policy_logits)
+        main_policy_logits = hk.Flatten()(main_policy_logits)
+        main_policy_logits = hk.Linear(self.num_actions)(main_policy_logits)
+
+        # exploration policy head
+        exploration_policy_logits = hk.Conv2D(output_channels=2, kernel_shape=1)(x)
+        exploration_policy_logits = hk.BatchNorm(True, True, 0.9)(
+            exploration_policy_logits, is_training, test_local_stats
+        )
+        exploration_policy_logits = jax.nn.relu(exploration_policy_logits)
+        exploration_policy_logits = hk.Flatten()(exploration_policy_logits)
+        exploration_policy_logits = hk.Linear(self.num_actions)(exploration_policy_logits)
+
+        # value head
+        v = hk.Conv2D(output_channels=1, kernel_shape=1)(x)
+        v = hk.BatchNorm(True, True, 0.9)(v, is_training, test_local_stats)
+        v = jax.nn.relu(v)
+        v = hk.Flatten()(v)
+        v = hk.Linear(self.num_channels)(v)
+        v = jax.nn.relu(v)
+        v = hk.Linear(1)(v)
+        v = jnp.tanh(v)
+        v = v.reshape((-1,))
+
+        # ube head
+        u = hk.Conv2D(output_channels=1, kernel_shape=1)(x)
+        u = hk.BatchNorm(True, True, 0.9)(u, is_training, test_local_stats)
+        u = jax.nn.relu(u)
+        u = hk.Flatten()(u)
+        u = hk.Linear(self.num_channels)(u)
+        u = jax.nn.relu(u)
+        u = hk.Linear(1)(u)
+        u = jnp.tanh(u)
+        u = u.reshape((-1,))
+
+        return main_policy_logits, exploration_policy_logits, v, u
+
+
 class FullyConnectedAZNet(hk.Module):
     """Fully-connected AlphaZero NN architecture."""
 
